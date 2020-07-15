080972c1293bdb47a134cecbebd742c7c204b0f2
Victor Johan <vicjohan23@gmail.com>
2020-07-15 14:13:33 -0400

    SE CREO LA ENTIDAD Clientes.

---
+++ Entidades/Clientes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ProyectoFinalServicioCliente.Entidades
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public int UsuarioId { get; set; }
+        [ForeignKey("UsuarioId")]
+        public virtual Usuarios Usuario { get; set; }
+        public string Nombres { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+    }
+}
