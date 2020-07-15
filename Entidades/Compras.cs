03c342e35a9ee88f77008930d42365a1e0ec92fb
Victor Johan <vicjohan23@gmail.com>
2020-07-15 01:14:41 -0400

    SE CREO LA ENNTIDAD Compras.

---
+++ Entidades/Compras.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ProyectoFinalServicioCliente.Entidades
+{
+    public class Compras
+    {
+        [Key]
+        public int CompraId { get; set; }
+        public int UsuarioId { get; set; }
+        public double Monto { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+    }
+}
