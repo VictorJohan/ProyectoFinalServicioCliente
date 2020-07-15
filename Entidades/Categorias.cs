6c47f21d6550808e05d80989beca25a662e7acc7
Victor Johan <vicjohan23@gmail.com>
2020-07-15 02:05:27 -0400

    PENDIENTE: CREAR LAS PROPIEDADES DE LA CLASE CATEGORIAS.

---
+++ Entidades/Categorias.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalServicioCliente.Entidades
+{
+    public class Categorias
+    {
+        //todo: Para Johan: Crear las propiedades de la clase Categoria e investigar si tiene que haber una foreign key de Usuarios en cada clase
+    }
+}
