27867d4d1d4df947735b1cf2541ae5aa0c5402df
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-07-14 22:03:50 -0400

    no me fuciona el inter no puedo istalar paquetes

---
+++ DAL/Contexto.cs
@@ -0,0 +1,33 @@
+﻿using ProyectoFinalServicioCliente.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalServicioCliente.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Combinaciones> Combinaciones { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging().UseSqlite(@"Data Source= DATA\TeacherControl.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<Usuarios>().HasData(new Usuarios
+            {
+                UsuarioId = 1,
+                Nombres = "Elvis J.",
+                Apellidos = "Duarte V.",
+                NombreUsuario = "admin",
+                Contrasena = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"
+            });
+        }
+    }
+}
---
+++ UI/Login/Login.xaml.cs
@@ -0,0 +1,55 @@
+﻿//using IdentityModel;
+using ProyectoFinalServicioCliente.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinalServicioCliente.UI.Login
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        //Metodo que ayuda a cerrar el programa si pone una contraseña mal o NombreUsuario
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            Application.Current.Shutdown();
+        }
+
+
+        private void IngresarButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool paso = LoginBLL.Validar(NombreUsuarioTextBox.Text,ContrasenaPasswordBox.Password);
+
+            if (paso)
+            {
+                this.Close();
+                Principal.Show();
+            }
+            else
+            {
+                MessageBox.Show("Error Nombre Usuario o Contraseña incorrecta!", "Error!");
+                ContrasenaPasswordBox.Clear();
+                NombreUsuarioTextBox.Focus();
+            }
+        }
+    }
+}
+
+
