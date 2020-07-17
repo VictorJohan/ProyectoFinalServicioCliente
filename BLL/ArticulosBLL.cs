2ddf67ff866d9fd7620d9eadd85012bad93efd61
Victor Johan <vicjohan23@gmail.com>
2020-07-17 19:34:26 -0400

    SE CREO ArticulosBLL

---
+++ BLL/ArticulosBLL.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalServicioCliente.DAL;
+using ProyectoFinalServicioCliente.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoFinalServicioCliente.BLL
+{
+    public class ArticulosBLL
+    {
+        public static bool Guardar(Articulos articulo)
+        {
+            if (!Existe(articulo.ArticuloId))
+                return Insertar(articulo);
+            else
+                return Modificar(articulo);
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                ok = contexto.Articulos.Any(a => a.ArticuloId == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Insertar(Articulos articulo)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                contexto.Articulos.Add(articulo);
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Modificar(Articulos articulo)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                contexto.Entry(articulo).State = EntityState.Modified;
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        public static Articulos  Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Articulos articulos;
+
+            try
+            {
+                articulos = contexto.Articulos.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return articulos;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                var eliminar = contexto.Articulos.Find(id);
+                if (eliminar != null)
+                {
+                    contexto.Articulos.Remove(eliminar);
+                    ok = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        public static List<Articulos> GetListArticulos()
+        {
+            Contexto contexto = new Contexto();
+            List<Articulos> lista = new List<Articulos>();
+
+            try
+            {
+                lista = contexto.Articulos.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
