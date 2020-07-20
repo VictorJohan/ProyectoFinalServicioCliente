5037ef240565cde12fc2cfecaa716122644faf05
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-07-20 17:34:51 -0400

    VentasBLL lista

---
+++ BLL/VentasBLL.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalServicioCliente.DAL;
+using ProyectoFinalServicioCliente.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ProyectoFinalServicioCliente.BLL
+{
+    public class VentasBLL
+    {
+        public static bool Guardar(Ventas venta)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.Ventas.Add(venta) != null)
+                    paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Ventas venta)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Database.ExecuteSqlRaw($"Delete FROM VentasDetalle Where VentasId={venta.VentaId}");
+                foreach (var item in venta.VentasDetalles)
+                {
+                    db.Entry(item).State = EntityState.Added;
+                }
+                db.Entry(venta).State = EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static Ventas Buscar(int id)
+        {
+            Ventas venta = new Ventas();
+            Contexto db = new Contexto();
+
+            try
+            {
+                venta = db.Ventas.Include(x => x.VentasDetalles).Where(x => x.VentaId == id).SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return venta;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                var eliminar = VentasBLL.Buscar(id);
+                db.Entry(eliminar).State = EntityState.Deleted;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static List<Ventas> GetList(Expression<Func<Ventas, bool>> venta)
+        {
+            List<Ventas> Lista = new List<Ventas>();
+            Contexto db = new Contexto();
+
+            try
+            {
+                Lista = db.Ventas.Where(venta).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
