579de86cf8e5a9b33efcca41d27bda0553b66794
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-07-25 09:55:56 -0400

    Diseño de ventana Eventos Listo

---
+++ UI/rEventos/rEventos.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinalServicioCliente.UI.rEventos
+{
+    /// <summary>
+    /// Interaction logic for rEventos.xaml
+    /// </summary>
+    public partial class rEventos : Window
+    {
+        public rEventos()
+        {
+            InitializeComponent();
+        }
+
+        private void EventoIdTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
