18d1707d73e0b39ed26422ff7f096730064de0a4
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-07-29 17:44:09 -0400

    Cunsulta Compras Lista

---
+++ UI/Consultas/cCompras.xaml.cs
@@ -0,0 +1,89 @@
+﻿using ProyectoFinalServicioCliente.BLL;
+using ProyectoFinalServicioCliente.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinalServicioCliente.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cCompras.xaml
+    /// </summary>
+    public partial class cCompras : Window
+    {
+        public cCompras()
+        {
+            InitializeComponent();
+        }
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var Listado = new List<Compras>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0://Todo
+                        Listado = ComprasBLL.GetList(s => true);
+                        break;
+                    case 1:
+                        try
+                        {
+                            int id = Convert.ToInt32(CriterioTextBox.Text);
+                            Listado = ComprasBLL.GetList(c => c.CompraId == id);
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Por favor, ingrese un ID valido");
+                        }
+                        break;
+                    case 2:
+                        try
+                        {
+                            int id = Convert.ToInt32(CriterioTextBox.Text);
+                            Listado = ComprasBLL.GetList(c => c.UsuarioId == id);
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Por favor, ingrese un ID valido");
+                        }
+                        break;
+                    case 3:
+                        try
+                        {
+                            double monto = Convert.ToDouble(CriterioTextBox.Text);
+                            Listado = ComprasBLL.GetList(c => c.Monto == monto);
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Por favor, ingrese un Critero valido");
+                        }
+                        break;
+
+                }
+
+                if (DesdeDataPicker.SelectedDate != null && HastaDatePicker.SelectedDate != null)
+                    Listado = Listado.Where(c => c.Fecha.Date >= DesdeDataPicker.SelectedDate.Value && c.Fecha.Date <= HastaDatePicker.SelectedDate.Value).ToList();
+
+            }
+
+
+            else
+            {
+                Listado = ComprasBLL.GetList(c => true);
+            }
+
+            ConsultaDataGrid.ItemsSource = null;
+            ConsultaDataGrid.ItemsSource = Listado;
+        }
+    }
+}
